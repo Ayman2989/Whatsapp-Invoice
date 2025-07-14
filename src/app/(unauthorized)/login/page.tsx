3c6ef351dd606e0da370afea4af6b5cf2565be33
Ayman Ashraf <Ayman2989>
2025-07-14 19:12:24 +0300

    login page change

--- src/app/(unauthorized)/login/page.tsx
+++ src/app/(unauthorized)/login/page.tsx
@@ -39,10 +39,10 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-slate-800 to-gray-900 flex items-center justify-center px-4">
+    <div className="min-h-screen bg-gradient-to-br from-primary via-orange-800 to-primary flex items-center justify-center px-4">
       <div className="bg-white rounded-xl shadow-lg w-full max-w-md p-8">
-        <h1 className="text-3xl font-bold text-gray-800 mb-6 text-center">
-          Welcome Back
+        <h1 className="text-3xl font-bold text-primary mb-6 text-center">
+          Whatsapp Businness Manager
         </h1>
 
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
@@ -53,7 +53,7 @@ const LoginPage = () => {
             <input
               type="email"
               {...register("email", { required: "Email is required" })}
-              className="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-indigo-500 outline-none"
+              className="w-full mt-1 text-gray-800 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-primary outline-none"
               placeholder="you@example.com"
             />
             {errors.email && (
@@ -70,7 +70,7 @@ const LoginPage = () => {
             <input
               type="password"
               {...register("password", { required: "Password is required" })}
-              className="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-indigo-500 outline-none"
+              className="w-full mt-1 px-4 text-gray-800 py-2 border rounded-lg focus:ring-2 focus:ring-primary outline-none"
               placeholder="••••••••"
             />
             {errors.password && (
@@ -85,7 +85,7 @@ const LoginPage = () => {
           <button
             type="submit"
             disabled={loading}
-            className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition-all font-semibold"
+            className="w-full bg-primary text-white py-2 rounded-lg duration-200 hover:bg-orange-800 transition-all font-semibold"
           >
             {loading ? "Logging in..." : "Login"}
           </button>
